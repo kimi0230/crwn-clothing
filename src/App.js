a5105128a269b79d0e483494c653222b852bc0e1
kimi <kimi0230@gmail.com>
2021-09-15 17:11:23 +0800

    header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./views/homepage/homepage";
 import ShopPage from "./views/shop/shop";
+import Header from "./components/header/header.component.js";
 import "./App.css";
 
 const App = () => (
   <BrowserRouter>
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   </BrowserRouter>
