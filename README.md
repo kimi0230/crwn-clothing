333f76057bec3602242a2a2f240b8648dff4860c
kimi <kimi0230@gmail.com>
2021-09-15 16:12:47 +0800

    format

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 npx create-react-app crwn-clothing --template cra-template-
 ```
 
+
 * https://github.com/ZhangMYihua
+* [cubic-bezier](https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function)
+* [react router](https://reactrouter.com/web/guides/quick-start)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.css';
-import HomePage from './views/homepage/homepage';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./views/homepage/homepage";
+import ShopPage from "./views/shop/shop";
+import "./App.css";
 
-function App() {
-  return <div>
-    <HomePage/>
-  </div>
-}
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={ShopPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
