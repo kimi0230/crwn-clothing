29d44c051485df8e9a9d5b4f3fe87b74266b1396
kimi <kimi0230@gmail.com>
2021-09-28 14:30:32 +0800

    feat: update cart reselect

--- src/components/cart_dropdown/cart_dropdown.component.js
+++ src/components/cart_dropdown/cart_dropdown.component.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CustomButton from "../custom_button/custom_button.component";
 import CartItem from "../cart_item/cart_item.componemt";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import "./cart_dropdown.styles.scss";
 
@@ -20,6 +21,8 @@ const CartDropdown = ({ cartItems }) => (
 // 將 redux的 state 給 componet的 props
 // https://react-redux.js.org/using-react-redux/connect-mapstate
 // ({ cart: { cartItems } })} 的 cart 是 root_reducer.js 的 cart
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
 
 export default connect(mapStateToProps)(CartDropdown);
