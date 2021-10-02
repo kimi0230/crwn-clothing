3fb2fb778dc4b83126d4e025968eed3921b4c578
kimiimac <kimi0230@gmail.com>
2021-10-02 18:52:05 +0800

    feat: base route path

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div>
           <Header />
           <Switch>
