b629cf33e60f7971a23c933153784522021d1a3a
kimi <kimi0230@gmail.com>
2021-10-05 14:52:36 +0800

    feat: get shop collection from firebase, then update redux

--- src/views/shop/shop.jsx
+++ src/views/shop/shop.jsx
@@ -40,7 +40,8 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  updateCollections: (collectionsMap) => dispatch(updateCollections),
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
