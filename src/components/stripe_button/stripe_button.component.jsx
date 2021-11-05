9993d9ab0200bb1fe0a703d9b2a16f2f6f86bc67
kimi <kimi0230@gmail.com>
2021-11-05 17:45:15 +0800

    feat: axiox

--- src/components/stripe_button/stripe_button.component.jsx
+++ src/components/stripe_button/stripe_button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -8,7 +9,23 @@ const StripeCheckoutButton = ({ price }) => {
   // callback
   const onToken = (token) => {
     console.log(token);
-    alert("Payment Succesful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
