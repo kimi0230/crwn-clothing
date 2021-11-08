8b635c56ca9b9fa07244d7a7b6adbbb8fb23af80
kimi <kimi0230@gmail.com>
2021-11-08 16:08:57 +0800

    feat: create context

--- src/views/collection/collection.jsx
+++ src/views/collection/collection.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import CollectionItem from "../../components/collection_item/collection_item.component";
 
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionsContext from "../../contexts/collections/collections.context";
 
 import "./collection.styles.scss";
 
 // 商品分類頁面 : /crwn-clothing/shop/hats
-const CollectionPage = () => {
-  const { collectionId } = useParams();
-  const collection = useSelector(selectCollection(collectionId));
-  const { title, items } = collection;
+const CollectionPage = ({ match }) => {
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
-        {items.map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    <CollectionsContext.Consumer>
+      {(collections) => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <div className="collection-page">
+            <h2 className="title">{title}</h2>
+            <div className="items">
+              {items.map((item) => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </div>
+        );
+      }}
+    </CollectionsContext.Consumer>
   );
 };
 
