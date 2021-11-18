b9c9957b2d5763a6db8cc299334eb4eb0231def3
kimi <kimi0230@gmail.com>
2021-11-18 14:39:52 +0800

    feat: graphql

---
+++ src/views/shop/shop_gql.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { default as CollectionsOverviewContainerGQL } from "../../components/collections_overview/collections_overview.container_gql";
+import CollectionPageContainer from "../collection/collection.container";
+
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainerGQL}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      ></Route>
+    </div>
+  );
+};
+
+export default ShopPage;
