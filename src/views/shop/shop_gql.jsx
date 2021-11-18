84812817fddeddd5ba88ea94287f698ee86cd53d
kimi <kimi0230@gmail.com>
2021-11-18 17:31:53 +0800

    feat: graphql

--- src/views/shop/shop_gql.jsx
+++ src/views/shop/shop_gql.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#importing_defaults
+// https://openhome.cc/Gossip/ECMAScript/Export.html
 import { default as CollectionsOverviewContainerGQL } from "../../components/collections_overview/collections_overview.container_gql";
+
 import CollectionPageContainer from "../collection/collection.container";
 
 const ShopPage = ({ match }) => {
