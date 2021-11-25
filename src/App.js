c84a43e7d520e2cfc8f2f2c0faf6ed570e70b497
kimi <kimi0230@gmail.com>
2021-11-25 17:56:32 +0800

    feat: lazy reload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser, checkUserSession } from "./redux/user/user.actions";
 
 import Header from "./components/header/header.component";
+import Spinner from "./components/spinner/spinner.component";
 // import { default as HeaderGQL } from "./components/header/header.container_gql";
-import HomePage from "./views/homepage/homepage";
-import ShopPage from "./views/shop/shop";
-import ShopPageGQL from "./views/shop/shop_gql";
-import CheckoutPage from "./views/checkout/checkout";
-import SignInSignUpPage from "./views/signin_signup/signin_signup";
 
 // import "./App.css";
 import { GlobalStyle } from "./global.styles";
+import SignInSignUpPage from "./views/signin_signup/signin_signup";
+
+const HomePage = lazy(() => import("./views/homepage/homepage"));
+const ShopPage = lazy(() => import("./views/shop/shop"));
+const ShopPageGQL = lazy(() => import("./views/shop/shop_gql"));
+const CheckoutPage = lazy(() => import("./views/checkout/checkout"));
+// const SignInSignUpPage = lazy(() =>
+//   import("./views/signin_signup/signin_signup")
+// );
 
 const App = () => {
   const currentUser = useSelector(setCurrentUser);
@@ -29,23 +34,25 @@ const App = () => {
         <Header />
         {/* <HeaderGQL /> */}
         <Switch>
-          {/* exact控制匹配到/路徑時不會再繼續向下匹配 */}
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/shopGQL" component={ShopPageGQL} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              currentUser ? (
-                // 登入後跳轉至首頁
-                <Redirect to="/" />
-              ) : (
-                <SignInSignUpPage />
-              )
-            }
-          />
-          <Route exact path="/checkout" component={CheckoutPage} />
+          <Suspense fallback={<Spinner />}>
+            {/* exact控制匹配到/路徑時不會再繼續向下匹配 */}
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route path="/shopGQL" component={ShopPageGQL} />
+            <Route
+              exact
+              path="/signin"
+              render={() =>
+                currentUser ? (
+                  // 登入後跳轉至首頁
+                  <Redirect to="/" />
+                ) : (
+                  <SignInSignUpPage />
+                )
+              }
+            />
+            <Route exact path="/checkout" component={CheckoutPage} />
+          </Suspense>
         </Switch>
       </div>
     </BrowserRouter>
